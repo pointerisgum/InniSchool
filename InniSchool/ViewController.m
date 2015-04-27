9ec66f51c94493a700f763d8aea26ceb04e72b59
KimYoung-Min <kimyoung-min@KimYoung-Minui-MacBook-Pro.local>
2015-04-27 16:52:38 +0900

    Initial Commit

---
+++ InniSchool/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  InniSchool
+//
+//  Created by KimYoung-Min on 2015. 4. 27..
+//  Copyright (c) 2015년 youngmin.kim. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ InniSchoolTests/InniSchoolTests.m
@@ -0,0 +1,34 @@
+//
+//  InniSchoolTests.m
+//  InniSchoolTests
+//
+//  Created by KimYoung-Min on 2015. 4. 27..
+//  Copyright (c) 2015년 youngmin.kim. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface InniSchoolTests : XCTestCase
+
+@end
+
+@implementation InniSchoolTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
